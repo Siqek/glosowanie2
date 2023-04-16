d4b79c4941da857d259d2902f03f26c2131c97d7
Siqek <113335428+Siqek@users.noreply.github.com>
2023-04-16 16:49:23 +0200

    .

--- client_admin/admin/chart.js
+++ client_admin/admin/chart.js
@@ -1,5 +1,5 @@
-var xValues = ''
-var yValues = ''
+var xValues = []
+var yValues = []
 var json = []
 var json2 = []
 var n = 0
@@ -33,43 +33,22 @@ function chart_n(){
     console.log(json)
     console.log(json2)
 
-    yValues = '['
-    xValues = '['
+    //yValues = '['
+    //xValues = '['
 
     for (var i=0; i<=json.length - 1; i++){
-        xValues = `${xValues}"${json[i].Name} ${json[i].Surname}"`
         n = 0
 
         for (var j=0; j<=json2.length - 1; j++){      
             if (json2[j].Candidate == i + 1) n++
         }
-        yValues = `${yValues}${n}`
-        if (i <= json.length - 2){
-            xValues = `${xValues}, `
-            yValues = `${yValues}, `
-        }
 
-        if (i == json.length - 1){
-            xValues = `${xValues}]`
-            yValues = `${yValues}]`
-        }
-    }//push
-    //obiekt
-    // ^
-    //string
-    //
-    ///
-    ///
-    ////
-    ///
-    ///
-    //
-    //
-    //
-    //
-    chart_n()
+        xValues.push(`${json[i].Name} ${json[i].Surname}`)
+        yValues.push(n)
+    }
     console.log(yValues)
     console.log(xValues)
+    chart_n()
   }
 
   getData()
