546d2d0235cefcfb17d1907036c6716881e7cf11
Siqek <113335428+Siqek@users.noreply.github.com>
2023-04-16 13:36:37 +0200

    .

--- client_admin/admin/chart.js
+++ client_admin/admin/chart.js
@@ -2,23 +2,26 @@ var xValues = ''
 var yValues = ''
 var json = []
 var json2 = []
+var n = 0
 
-new Chart("myChart", {
-    type: "pie",
-    data: {
-      labels: ["red", "dom"],
-      datasets: [{
-        backgroundColor: "red",
-        data: "yValues"
-      }]
-    },
-    options: {
-      title: {
-        display: true,
-        text: "World Wide Wine Production"
-      }
+function chart_n(){
+    new Chart("myChart", {
+        type: "bar",
+        data: {
+          labels: xValues,
+          datasets: [{
+            backgroundColor: "olive",
+            data: yValues
+          }]
+        },
+        options: {
+          title: {
+            display: true,
+            text: "World Wide Wine Production"
+          }
+        }
+      });
     }
-  });
 
   async function getData(){
     const data = await fetch(`${baseurl}/select/candidates`)
@@ -32,14 +35,28 @@ new Chart("myChart", {
 
     yValues = '['
     xValues = '['
-    
+
     for (var i=0; i<=json.length - 1; i++){
-        console.log(i)
+        xValues = `${xValues}"${json[i].Name} ${json[i].Surname}"`
+        n = 0
+
         for (var j=0; j<=json2.length - 1; j++){      
-            console.log(j)
+            if (json2[j].Candidate == i + 1) n++
+        }
+        yValues = `${yValues}${n}`
+        if (i <= json.length - 2){
+            xValues = `${xValues}, `
+            yValues = `${yValues}, `
+        }
+
+        if (i == json.length - 1){
+            xValues = `${xValues}]`
+            yValues = `${yValues}]`
         }
     }
-    
+    chart_n()
+    console.log(yValues)
+    console.log(xValues)
   }
 
   getData()
